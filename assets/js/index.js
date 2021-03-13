618d87d36b42216648eafff3ee5562406ea49161
Quict <1216893318@qq.com>
2021-03-13 21:37:17 +0800

    完成首页获取用户信息,并退出登录

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+  // 获得用户信息
+  function getUserInfo() {
+    // 发送ajax
+    axios
+      .get("http://api-breakingnews-web.itheima.net/my/userinfo", {
+        headers: { Authorization: localStorage.getItem("token") },
+      })
+      .then(function (res) {
+        let data = res.data.data;
+        let name = data.nickname || data.username;
+        if (data.user_pic !== null) {
+          $(".layui-nav-img").attr("url", data.user_pic).show();
+          $(".text-avatar-box").hide();
+        } else {
+          $(".layui-nav-img").hide();
+          $(".text-avatar-box").show().children().text(name[0].toUpperCase());
+        }
+        $("#welcome").text("欢迎 " + name);
+      });
+  }
+
+  getUserInfo();
+
+  // 点击退出
+  $("#btnLogout").on("click", function () {
+    layer.confirm("确定退出嘛?", { icon: 3, title: "提示" }, function (index) {
+      localStorage.removeItem("token");
+      location.href = "/login.html";
+      layer.close(index); // 关闭窗口
+    });
+  });
+});
